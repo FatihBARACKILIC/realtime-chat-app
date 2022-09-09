fade17603879cb7b76ec05c38a2651dd3b5770a7
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2022-09-09 18:53:00 +0300

    backend

---
+++ backend/clients/redis.js
@@ -0,0 +1,18 @@
+const redis = require("redis")
+
+/*
+const getClient = () => {
+  return redis.createClient(process.env.REDIS_HOST)
+}
+*/
+
+const getClient = () => {
+  return redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASS,
+    db: 1,
+  })
+}
+
+module.exports.getClient = getClient
