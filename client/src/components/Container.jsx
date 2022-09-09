ae5b71f623fb02ee7f24d2f63254e2c921bc888f
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2022-09-09 18:53:06 +0300

    client

---
+++ client/src/components/Container.jsx
@@ -0,0 +1,13 @@
+import ChatForm from "./ChatForm"
+import ChatList from "./ChatList"
+
+const Container = () => {
+  return (
+    <>
+      <ChatList />
+      <ChatForm />
+    </>
+  )
+}
+
+export default Container
